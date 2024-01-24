a7f2c6e4bc4e5b5ffa6dc8600c024ad3f55080e5
Diego Cuenca <dcuenca@lorente.fin.ec>
2024-01-24 10:57:03 -0500

    first commit

---
+++ src/main/java/com/environment/persistence/Employee.java
@@ -0,0 +1,20 @@
+package com.environment.persistence;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    
+    @Id
+    private Long id;
+    private String name;
+}
---
+++ src/test/java/com/enviroment/springenvironment/SpringEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviroment.springenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
